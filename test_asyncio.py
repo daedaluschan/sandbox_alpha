8e65b70c3c46c2e19bce3b52af67d38b9bf2f474
U425651 <daedalus.chan@wellsfargo.com>
2016-06-15 17:03:48 +0800

    coroutine

--- test_asyncio.py
+++ test_asyncio.py
@@ -1,26 +1,8 @@
-from functools import wraps
-from time import time
-
-
-def log_execution_time(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time()
-        return_value = func(*args, **kwargs)
-        message = "Executing {} took {:.03} seconds.".format(func.__name__, time() - start)
-        print(message)
-        return return_value
-    return wrapper
-
-def fib(n):
-    return fib(n - 1) + fib(n - 2) if n > 1 else n
-
-
-timed_fib = log_execution_time(fib)
 
 import selectors
 import sys
 from time import time
+from fib import timed_fib
 
 
 def process_input(stream):
